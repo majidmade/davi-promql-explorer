95036b408747699857716951afe3d2a68454bee0
Majid Razvi <mrazvi@pivotal.io>
2019-11-26 15:10:27 -0700

    set up davi scaffolding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from 'pivotal-ui/react/theme-context'
+import { Input } from 'pivotal-ui/react/inputs';
+import { SyncProvider, Container, StepChart } from 'davi-js';
+
+const containerTitle = { 
+  text: <Input style={{ width: '100%' }} placeholder="Search" icon="search" /> 
+};
+
+const useFetch = query => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [data, setData] = useState([])
+  return { loading, error, data }
+}
 
 function App() {
+  const { loading, error, data } =  useFetch()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ width: '90%' }}>
+          <ThemeProvider theme="dark">
+            <Container title={containerTitle} loading={true}>
+              <SyncProvider>
+                {sync => (
+                  <StepChart
+                    {...sync}
+                    zoomEnabled
+                    generateFakeData={true}
+                    data={data}
+                  />
+                )}
+              </SyncProvider>
+            </Container>
+          </ThemeProvider>
+        </div>
       </header>
     </div>
   );
